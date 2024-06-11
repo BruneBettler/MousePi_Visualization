d9462a85cd9e7899f4125e8c74dc24062991fdd4
Brune Bettler <brune.bettler@mail.mcgill.ca>
2024-06-11 13:56:08 -0400

    Fixed bug in run.py add_new_session

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ DO NOT include a last / when indicating a folder in path. ex. ('/Volumes/MATT_1/
 params = {
     'path_to_all': '/Volumes/MATT_1/behavior/Pilot_5_june_10/Phase 1',
     'path_to_session': '/Volumes/MATT_1/behavior/Pilot_5_june_10/Phase 1/06-10',
-    'session_numbers': [13,13,8,8],
+    'session_numbers': [13,13,8,8], # add the session number of the new session you are adding in the order of the mice_to_analyze list below
     'mice_to_analyze': ['OLAF','THOR', 'POPPY', 'FANNY'], # names must be in all caps; inlcude only one name for single mouse analysis
     'phase': 1
         }
@@ -42,9 +42,11 @@ def pipeline_0():
     return 0
 
 def add_new_session():
+    all_mice_data = get_multiDay_mice_data(params['mice_to_analyze'], path=params['path_to_all'])
+    clean_mice_data = []
     session_date = params['path_to_session'][-5:]
-    mice_data = []
     for mouse_n, mouse in enumerate(params['mice_to_analyze']):
+        clean_mice_data.append(all_mice_data[mouse_n][0])
         for _, _, files in os.walk(params['path_to_session']):
             for file_name in files:
                 if file_name[-4:] == '.log' and file_name[:len(mouse)] == mouse:
@@ -52,13 +54,12 @@ def add_new_session():
                     break
 
         mouse_data = single_day_data(path=file)
-        mice_data.append(mouse_data)
         graph_all_singleMouse(mouse_data, f"{params['mice_to_analyze'][mouse_n]}_Single_Day_Analysis_{session_date}", params['phase'], params['session_numbers'][mouse_n], 15, save_path=params['path_to_all'])
         print( f"{datetime.now()}: graph_all_singleMouse for {mouse}, session: {session_date} is DONE!")
         goNogo_lick_analysis_single_mouse(mouse_data, f"{mouse}_Single_Day_Analysis_{session_date}", params['phase'], params['session_numbers'][mouse_n], save_path=params['path_to_all'])
         print(f"{datetime.now()}:graph_GOnoGO for {mouse}, session: {session_date} is DONE!")
 
-    graph_all_MultiMice(mice_data, params['mice_to_analyze'], params['phase'], save_path=params['path_to_all'])
+    graph_all_MultiMice(clean_mice_data, params['mice_to_analyze'], params['phase'], save_path=params['path_to_all'])
     print(f"{datetime.now()}: graph_all_MultiMouse for {params['mice_to_analyze']} is DONE!")
     print(f"{datetime.now()}: All Done!")
     return 0
