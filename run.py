e48a4b993901dbc7bc757ff3e7dd0b9bdfde5038
Brune Bettler <brune.bettler@mail.mcgill.ca>
2024-06-11 13:20:04 -0400

    Modified run.py to allow for addition of single session graphing

--- run.py
+++ run.py
@@ -4,11 +4,13 @@ from datetime import datetime
 
 
 '''
-Script makes and saves all desired graphs for all desired mice as 
-specified below
+Script makes and saves all desired graphs for all desired mice as specified below. 
+DO NOT include a last / when indicating a folder in path. ex. ('/Volumes/MATT_1/behavior/Pilot_5_june_10/Phase 1')
 '''
 params = {
-    'path': '/Volumes/MATT_1/behavior/Pilot_5_june_7/Phase 1',
+    'path_to_all': '/Volumes/MATT_1/behavior/Pilot_5_june_10/Phase 1',
+    'path_to_session': '/Volumes/MATT_1/behavior/Pilot_5_june_10/Phase 1/06-10',
+    'session_numbers': [13,13,8,8],
     'mice_to_analyze': ['OLAF','THOR', 'POPPY', 'FANNY'], # names must be in all caps; inlcude only one name for single mouse analysis
     'phase': 1
         }
@@ -20,7 +22,7 @@ Simply click the green arrow and see the chosen graphs in the graph folder once 
 def pipeline_0():
     # go through all different session folders in the path
     # get all data for every mouse across sessions
-    all_data = get_multiDay_mice_data(params['mice_to_analyze'], path=params['path'])
+    all_data = get_multiDay_mice_data(params['mice_to_analyze'], path=params['path_to_all'])
     mice_data = []
     # run single day analysis for all mice
     for mouse_n, mouse_data in enumerate(all_data):
@@ -28,16 +30,38 @@ def pipeline_0():
         mice_data.append(data)
         sessions = mouse_data[1]
         for n, session in enumerate(sessions):
-            graph_all_singleMouse(data[n], f"{params['mice_to_analyze'][mouse_n]}_Single_Day_Analysis_{session}", params['phase'], n+1, 15, save_path=params['path'])
+            graph_all_singleMouse(data[n], f"{params['mice_to_analyze'][mouse_n]}_Single_Day_Analysis_{session}", params['phase'], n+1, 15, save_path=params['path_to_all'])
             print(f"{datetime.now()}: graph_all_singleMouse for {params['mice_to_analyze'][mouse_n]}, session: {session} is DONE!")
-            goNogo_lick_analysis_single_mouse(data[n], f"{params['mice_to_analyze'][mouse_n]}_Single_Day_Analysis_{session}", params['phase'], n+1, save_path=params['path'])
+            goNogo_lick_analysis_single_mouse(data[n], f"{params['mice_to_analyze'][mouse_n]}_Single_Day_Analysis_{session}", params['phase'], n+1, save_path=params['path_to_all'])
             print(f"{datetime.now()}:graph_GOnoGO for {params['mice_to_analyze'][mouse_n]}, session: {session} is DONE!")
 
 
-    graph_all_MultiMice(mice_data, params['mice_to_analyze'], params['phase'], save_path=params['path'])
+    graph_all_MultiMice(mice_data, params['mice_to_analyze'], params['phase'], save_path=params['path_to_all'])
+    print(f"{datetime.now()}: graph_all_MultiMouse for {params['mice_to_analyze']} is DONE!")
+    print(f"{datetime.now()}: All Done!")
+    return 0
+
+def add_new_session():
+    mice_data = []
+    for mouse_n, mouse in enumerate(params['mice_to_analyze']):
+        for _, _, files in os.walk(params['path_to_session']):
+            for file_name in files:
+                if file_name[-4:] == '.log' and file_name[:len(mouse)] == mouse:
+                    file = params['path_to_session'] + '/' + file_name
+                    break
+
+        mouse_data = single_day_data(path=file)
+        mice_data.append(mouse_data)
+        graph_all_singleMouse(mouse_data, f"{params['mice_to_analyze'][mouse_n]}_Single_Day_Analysis_{params['session_numbers'][mouse_n]}", params['phase'], params['session_numbers'][mouse_n], 15, save_path=params['path_to_all'])
+        print( f"{datetime.now()}: graph_all_singleMouse for {mouse}, session: {params['session_numbers'][mouse_n]} is DONE!")
+        goNogo_lick_analysis_single_mouse(mouse_data, f"{mouse}_Single_Day_Analysis_{params['session_numbers'][mouse_n]}", params['phase'], params['session_numbers'][mouse_n], save_path=params['path_to_all'])
+        print(f"{datetime.now()}:graph_GOnoGO for {mouse}, session: {params['session_numbers'][mouse_n]} is DONE!")
+
+    graph_all_MultiMice(mice_data, params['mice_to_analyze'], params['phase'], save_path=params['path_to_all'])
     print(f"{datetime.now()}: graph_all_MultiMouse for {params['mice_to_analyze']} is DONE!")
     print(f"{datetime.now()}: All Done!")
     return 0
 
 if __name__ == "__main__":
-    pipeline_0()
+    #pipeline_0()
+    add_new_session()
